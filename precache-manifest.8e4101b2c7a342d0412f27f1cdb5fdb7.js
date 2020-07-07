d9cea60b2a6dea12005d67dff63c9ba3f483444b
Jovana Ilin <jovanailin@gmail.com>
2020-07-07 16:09:27 +0200

    Updates

---
+++ precache-manifest.8e4101b2c7a342d0412f27f1cdb5fdb7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e8d2f4c86fd08ba2b157302d59884fd",
+    "url": "/my-shop/index.html"
+  },
+  {
+    "revision": "b6cbb2fd3a2a28ad0e50",
+    "url": "/my-shop/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "1836a51cee0943758877",
+    "url": "/my-shop/static/css/main.01a1de46.chunk.css"
+  },
+  {
+    "revision": "b6cbb2fd3a2a28ad0e50",
+    "url": "/my-shop/static/js/2.9dcac4a9.chunk.js"
+  },
+  {
+    "revision": "7585d7e2191ead1c7fd01d7ebe90ade7",
+    "url": "/my-shop/static/js/2.9dcac4a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1836a51cee0943758877",
+    "url": "/my-shop/static/js/main.59f914d4.chunk.js"
+  },
+  {
+    "revision": "7ce8d71e725d3a76520b",
+    "url": "/my-shop/static/js/runtime-main.b7058dfc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-shop/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6c7b36b83c4340a93c04156063cbb58d",
+    "url": "/my-shop/static/media/logo.6c7b36b8.svg"
+  }
+]);
