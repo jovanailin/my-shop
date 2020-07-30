b6cf027a3b0f29e27e316d03a34daff97f334b6e
Jovana Ilin <jovanailin@gmail.com>
2020-07-30 13:17:01 +0200

    hashrouter instead of browserrouter

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from "react-router-dom"
+import {HashRouter} from "react-router-dom"
 import * as serviceWorker from './serviceWorker';
 import {ProductProvider} from "./context"
 import {createStore} from "redux"
 
 ReactDOM.render(
   <ProductProvider>
-    <BrowserRouter>
+    <HashRouter>
           <App></App>
-      </BrowserRouter>
+      </HashRouter>
   </ProductProvider>,
   document.getElementById('root')
 );
